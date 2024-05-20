24a2a017a0918a699edb987f00a9b532dc292145
erinengle2024 <158017994+erinengle2024@users.noreply.github.com>
2024-05-19 21:42:51 -0700

    leaflet part 1 final code

---
+++ Leaflet-Part-1/static/js/.ipynb_checkpoints/logic-checkpoint.js
@@ -0,0 +1,79 @@
+// Fetch earthquake data from USGS
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(function(data) {
+    // Create Leaflet map
+    var myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5
+    });
+
+    // Add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+    // Function to determine color based on depth
+    function getColor(d) {
+        return d > 100 ? '#FB5F66' :
+               d > 70  ? '#FCA35D' :
+               d > 50  ? '#FDB729' :
+               d > 30  ? '#F7DB11' :
+               d > 10  ? '#DCF401' :
+                         '#A3F600';
+    }
+
+    // Function to create legend
+    function createLegend() {
+        var legend = L.control({ position: 'bottomright' });
+        legend.onAdd = function (map) {
+            var div = L.DomUtil.create('div', 'info legend');
+            var depths = [0, 10, 30, 50, 70, 100];
+            div.innerHTML += '<b>Depth (km)</b><br>';
+            for (var i = 0; i < depths.length; i++) {
+                div.innerHTML +=
+                    '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
+                    (depths[i + 1] ? depths[i] + '&ndash;' + depths[i + 1] + '<br>' : depths[i] + '+');
+            }
+            return div;
+        };
+        legend.addTo(myMap);
+    }
+       
+
+    // Plot earthquake data
+    data.features.forEach(function(feature) {
+        // Extract necessary information from feature
+        var lat = feature.geometry.coordinates[1];
+        var lon = feature.geometry.coordinates[0];
+        var mag = feature.properties.mag;
+        var depth = feature.geometry.coordinates[2];
+        var place = feature.properties.place;
+
+        // Create marker options based on magnitude and depth
+        var markerOptions = {
+            radius: mag * 5,
+            fillColor: getColor(depth),
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        // Create marker and bind popup
+        L.circleMarker([lat, lon], markerOptions)
+            .bindPopup("Magnitude: " + mag + "<br>Location: " + place + "<br>Depth: " + depth + " km")
+            .addTo(myMap);
+    });
+
+    // Create legend
+    createLegend();
+});
+
+
+
+
+
+
+
+
+
+
